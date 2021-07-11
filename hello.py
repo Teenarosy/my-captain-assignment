41ab3d84f683dba431617cdc7a91f1369421c767
Teenarosy <86910567+Teenarosy@users.noreply.github.com>
2021-07-11 20:36:49 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 11 19:57:20 2021
+
+@author: Admin
+"""
+
+# Area of the circle
+
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
